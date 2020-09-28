0b48746ffac18fb1a247b4344f9ce77612188368
gabriel-fz <gabrielfzancanela@gmail.com>
2020-09-28 11:53:23 -0300

    backend: formatando com eslint

--- backend/app/Models/Team.js
+++ backend/app/Models/Team.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use("Model");
+const Model = use('Model');
 
 class Team extends Model {
   users() {
-    return this.belongsToMany("App/Models/User").pivotModel(
-      "App/Models/UserTeam"
+    return this.belongsToMany('App/Models/User').pivotModel(
+      'App/Models/UserTeam',
     );
   }
 }
--- backend/app/Models/User.js
+++ backend/app/Models/User.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use("Model");
+const Model = use('Model');
 
 /** @type {import('@adonisjs/framework/src/Hash')} */
-const Hash = use("Hash");
+const Hash = use('Hash');
 
 class User extends Model {
   static boot() {
@@ -14,7 +14,7 @@ class User extends Model {
      * A hook to hash the user password before saving
      * it to the database.
      */
-    this.addHook("beforeSave", async (userInstance) => {
+    this.addHook('beforeSave', async userInstance => {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password);
       }
@@ -22,12 +22,12 @@ class User extends Model {
   }
 
   tokens() {
-    return this.hasMany("App/Models/Token");
+    return this.hasMany('App/Models/Token');
   }
 
   teams() {
-    return this.belongsToMany("App/Models/Team").pivotModel(
-      "App/Models/UserTeam"
+    return this.belongsToMany('App/Models/Team').pivotModel(
+      'App/Models/UserTeam',
     );
   }
 }
