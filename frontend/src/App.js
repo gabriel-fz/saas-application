4e5180e5da716d71bb252e090a5b8c44b357c6ca
gabriel-fz <gabrielfzancanela@gmail.com>
2020-10-02 14:41:49 -0300

    frontend: configuraçao de rotas e redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Routes />
       <GlobalStyle />
-    </>
+    </Provider>
   );
 }
 
