6d489b8a5c2a760aefd6ea7794a9cff0c0e21b10
gabriel-fz <gabrielfzancanela@gmail.com>
2020-10-03 13:42:57 -0300

    frontend: layout do teamswitcher concluido

--- frontend/src/pages/Main/styles.js
+++ frontend/src/pages/Main/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  font-size: 24px;
-  color: #fff;
-  font-family: Arial, Helvetica, sans-serif;
+export const Container = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: stretch;
 `;
