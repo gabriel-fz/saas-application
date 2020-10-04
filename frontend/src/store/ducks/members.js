d14d34014fd0a10191a6d32ebcd10edfa7511f34
gabriel-fz <gabrielfzancanela@gmail.com>
2020-10-04 14:47:53 -0300

    frontend: listagem de membros concluido

--- frontend/src/store/ducks/members.js
+++ frontend/src/store/ducks/members.js
@@ -4,12 +4,15 @@ import Immutable from 'seamless-immutable';
 const { Types, Creators } = createActions({
   openMembersModal: null,
   closeMembersModal: null,
+  getMembersRequest: null,
+  getMembersSuccess: ['data'],
 });
 
 export const MembersTypes = Types;
 export default Creators;
 
 export const INITIAL_STATE = Immutable({
+  data: [],
   membersModalOpen: false,
 });
 
@@ -17,7 +20,10 @@ export const openModal = state => state.merge({ membersModalOpen: true });
 
 export const closeModal = state => state.merge({ membersModalOpen: false });
 
+export const getSuccess = (state, { data }) => state.merge({ data });
+
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.OPEN_MEMBERS_MODAL]: openModal,
   [Types.CLOSE_MEMBERS_MODAL]: closeModal,
+  [Types.GET_MEMBERS_SUCCESS]: getSuccess,
 });
