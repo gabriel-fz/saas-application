8f76eb8661479b2d0bd07e4d5af502db3688670c
gabriel-fz <gabrielfzancanela@gmail.com>
2020-10-05 13:22:39 -0300

    mobile: configuração inicial de rotas

---
+++ mobile/src/pages/SignIn/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles';
+
+const SignIn = () => <View style={styles.container} />;
+
+export default SignIn;
